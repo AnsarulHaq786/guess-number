6e0f2a218581140ca288fe333f8f9420c936a2fb
user.name "Ansarul Haq <mdansarul786940@gmail.com>
2025-03-05 09:54:35 +0545

    update element for ux

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
-let secretNumber=Math.floor(Math.random()*20+1);
+const secretNumber=Math.floor(Math.random()*100+1);
 let tryCount=0;
-function checkNum()
-{
+let userNumber=document.getElementById("input").value;
+let resultText=document.getElementById("result-text");
+const submitButton=document.getElementById("submit-button");
+let resultImg=document.getElementById("result-img");
+let result=document.getElementsByClassName("result");
+
+submitButton.addEventListener('click', ()=> {
     let userNumber=document.getElementById("input").value;
-        if(userNumber<secretNumber)
+    if(userNumber<secretNumber)
         {
             tryCount++;
-            document.getElementById("para").innerHTML=`Try Count=${tryCount}<br>Try greater number than this!`;
+            resultText.innerText="Oops your expectations are too low, try higher number!";
+            resultImg['src']="./images/cat-holding-uparrow-board.webp"
         }
             
         else if(userNumber>secretNumber)
         {
             tryCount++;
-            document.getElementById("para").innerHTML=`Try Count=${tryCount}<br>Try smaller number than this!`;
+            resultText.innerText="Oops you are over confident, try smaller number!";
+            resultImg['src']="./images/cat-holding-downarrow-board.png"
         }     
         else
         {
-            document.getElementById("para").innerHTML=`Congratulation you guessed the number in ${tryCount+1}th attempt!<br> Here is your gift`;
-            document.getElementById("giftButton").style.display="block";
+            resultText.innerText="Congratulations!, Accept the provided gift by clicking on the gift box.";
+            resultImg['src']="./images/cat-holding-giftbox.webp";
+            var aTag = document.createElement('a');
+            aTag.setAttribute('href',"https://youtu.be/xvFZjo5PgG0?autoplay=1");
+            result[0].appendChild(aTag);
+            aTag.appendChild(resultImg);
         }
+});
+
+function checkNum()
+{
+    
+        
 }
 function openGift()
 {
-    location.href="https://youtu.be/xvFZjo5PgG0?autoplay=1";
+    location.href="";
 }
